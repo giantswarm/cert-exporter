6afb16e2d280341e0d23964cab64f24e90235e92
Stevo Slavić <stevo@giantswarm.io>
2019-10-15 14:19:04 +0200

    Update to kubernetes-1.15.4 (#43)

---
+++ pkg/project/project.go
@@ -0,0 +1,29 @@
+package project
+
+var (
+	description = "The cert-exporter walks a directory path it has gotten as input and emits all NotAfter timestamps as metrics."
+	gitSHA      = "n/a"
+	name        = "cert-exporter"
+	source      = "https://github.com/giantswarm/cert-exporter"
+	version     = "n/a"
+)
+
+func Description() string {
+	return description
+}
+
+func GitSHA() string {
+	return gitSHA
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
