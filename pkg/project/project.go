a01c0757229282bfd1af3ac49bfdb18968ca45a1
Jakub Soboń <wtty.fool@gmail.com>
2020-05-15 18:07:59 +0200

    bump version to 1.2.4-dev (#59)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cert-exporter"
 	source      = "https://github.com/giantswarm/cert-exporter"
-	version     = "1.2.3"
+	version     = "1.2.4-dev"
 )
 
 func Description() string {
