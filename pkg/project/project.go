665f7f8e9f8eeaa5ef8dd03a8da456695fe3820f
GitHub Action <action@github.com>
2020-12-02 17:15:42 +0000

    Bump version to 1.4.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cert-exporter"
 	source      = "https://github.com/giantswarm/cert-exporter"
-	version     = "1.4.0"
+	version     = "1.4.1-dev"
 )
 
 func Description() string {
