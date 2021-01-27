ca57bd3eab454d818774ab551521ee1f8f54b0bd
GitHub Action <action@github.com>
2021-01-27 10:47:13 +0000

    Bump version to 1.6.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cert-exporter"
 	source      = "https://github.com/giantswarm/cert-exporter"
-	version     = "1.6.0"
+	version     = "1.6.1-dev"
 )
 
 func Description() string {
