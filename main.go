09b22db7f536232297855affaae67aa3216d820e
Gerald Pape <gerald@giantswarm.io>
2021-10-05 21:20:52 +0200

    add flags for disabling exporters

--- main.go
+++ main.go
@@ -36,16 +36,18 @@ func main() {
 	var tokenPath string
 	var vaultURL string
 	var help bool
-	var monitorSecrets bool
 	var monitorCertificates bool
+	var monitorFiles bool
+	var monitorSecrets bool
 	flag.StringVar(&address, "address", ":9005", "address which cert-exporter uses to listen and serve")
 	flag.StringVar(&certPaths, "cert-paths", "", "comma separated folders containing certs to export")
 	flag.StringVar(&namespaces, "namespaces", "", "comma separated namespaces in which to monitor TLS secrets")
 	flag.StringVar(&tokenPath, "token-path", "", "folder containing Vault tokens to export")
 	flag.StringVar(&vaultURL, "vault-url", "", "URL of Vault server")
 	flag.BoolVar(&help, "help", false, "print usage and exit")
-	flag.BoolVar(&monitorSecrets, "monitor-secrets", true, "monitor expiry of TLS secrets")
 	flag.BoolVar(&monitorCertificates, "monitor-certificates", true, "monitor expiry of cert-manager certificates")
+	flag.BoolVar(&monitorFiles, "monitor-files", true, "monitor expiry certificate files")
+	flag.BoolVar(&monitorSecrets, "monitor-secrets", true, "monitor expiry of Kubernetes TLS Secrets (type kubernetes.io/tls)")
 	flag.Parse()
 
 	if help {
@@ -53,7 +55,11 @@ func main() {
 		return
 	}
 
-	{
+	if !monitorCertificates && !monitorFiles && !monitorSecrets {
+		panic(microerror.Maskf(invalidConfigError, "all exporters are disabled"))
+	}
+
+	if monitorFiles {
 		if certPaths == "" {
 			panic(microerror.Maskf(invalidConfigError, "path to cert folder can not be empty"))
 		}
