e94ad60aef0dcae60bf61f021083f23cc63f54a4
taylorbot <dev@giantswarm.io>
2023-10-23 13:46:56 +0000

    Release v2.8.2

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cert-exporter"
 	source      = "https://github.com/giantswarm/cert-exporter"
-	version     = "2.8.2-dev"
+	version     = "2.8.2"
 )
 
 func Description() string {
