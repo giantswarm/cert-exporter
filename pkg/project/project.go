66718e3cadad59a84a3b7a0d189f004541c23a20
taylorbot <dev@giantswarm.io>
2026-05-10 10:43:10 +0000

    Bump version to 2.11.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cert-exporter"
 	source      = "https://github.com/giantswarm/cert-exporter"
-	version     = "2.11.0"
+	version     = "2.11.1-dev"
 )
 
 func Description() string {
