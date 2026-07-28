7a4a85f66c559b6d68c37a201e9770acf76d2939
taylorbot <dev@giantswarm.io>
2026-07-28 09:05:05 +0000

    chore: bump version to 2.11.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cert-exporter"
 	source      = "https://github.com/giantswarm/cert-exporter"
-	version     = "2.11.2"
+	version     = "2.11.3-dev"
 )
 
 func Description() string {
